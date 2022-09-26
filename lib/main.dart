136e7251e1f74444272160687795fe408b015802
nep-nep327 <91326644+nep-nep327@users.noreply.github.com>
2022-09-26 18:07:25 +0200

    [HWAN] Fixed like button

--- lib/main.dart
+++ lib/main.dart
@@ -48,16 +48,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _liked = 156;
 
-  void _incrementCounter() {
+  void _doALike() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _liked++;
     });
   }
 
@@ -151,17 +151,20 @@ class _MyHomePageState extends State<MyHomePage> {
                             ])),
                     Expanded(
                         flex: 1,
-                        child: Row(
+                        child: InkWell(
+                            child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.center,
-                            children: const [
+                            children: [
                               Icon(
                                 Icons.star,
                                 color: Colors.red,
                                 size: 24.0,
                               ),
-                              Text("156")
-                            ])),
+                              Text("${_liked}")
+                            ]),
+                          onTap: _doALike,
+                        )),
                   ],
                 )),
             Expanded(
@@ -230,7 +233,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _doALike,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
